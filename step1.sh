ff28f4eff522ffa7e9a1f3547ca219ab896911a8
陳崇光 <WillyChen@wrl88-108.nchc.org.tw>
2018-12-27 13:50:57 +0800

    Update Step2

--- step1.sh
+++ step1.sh
@@ -1,9 +1,19 @@
+for arg in "$@"
+do 
+   if [ "$arg" == "--path" ] || [ "$arg" == "-p" ]
+   then
+      COPYTOFILE="$2"
+   fi
+done
+echo "Start Process"
+tar -C ~/"$COPYTOFILE"  -xvzf ~/"$COPYTOFILE"/cifar-10-python.tar.gz
+
 echo "Checking for folder"
 if [ ! -d "~/.keras/datasets" ]; then
 echo "Creating folder"
 mkdir ~/.keras/datasets
 fi
 echo "Moving data to folder"
-cp -R ~/lustre/cifar-10-batches-py ~/.keras/datasets/
+cp -R ~/"$COPYTOFILE"/cifar-10-batches-py ~/.keras/datasets/
 echo "Finished moving data"
 
