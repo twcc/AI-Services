199ddde0d9a0584aabbaf6648b670542260776b7
Willy <taiwanpride888@gmail.com>
2019-01-02 06:17:42 +0000

    chagne

--- step2.sh
+++ step2.sh
@@ -1,9 +1,35 @@
+#!/bin/bash
+ar=($@)
+EPOCH=3
+BATCH=45
+for arg in "${!ar[@]}"
+do
+   if [ "${ar[$arg]}" == "--path" ] || [ "${ar[$arg]}" == "-p" ]
+   then
+      COPYTOFILE="${ar[$(($arg + 1))]}"
+   fi
+   if [ "${ar[$arg]}" == "--epoch" ] || [ "${ar[$arg]}" == "-e" ]
+   then
+      EPOCH="${ar[$(($arg + 1))]}"
+   fi
+   if [ "${ar[$arg]}" == "--batch" ] || [ "${ar[$arg]}" == "-b" ]
+   then
+      BATCH="${ar[$(($arg + 1))]}"
+   fi
+done
+
+if [ ! -d "/mnt/s3/$COPYTOFILE/weights/" ]; then
+echo "Creating weight folder"
+mkdir /mnt/s3/"$COPYTOFILE"/weights/
+fi
+
 cd inceptionv3/
-sudo pip install -r requirements.txt 
+sudo pip install -r requirements.txt >> install.log 
 
 cd train/
 
-python V3.py start-training
+echo "/mnt/s3/$COPYTOFILE/" 
+python V3.py start-training --batch $BATCH --epoch $EPOCH --path /mnt/s3/"$COPYTOFILE"/weights/
 
 
 
