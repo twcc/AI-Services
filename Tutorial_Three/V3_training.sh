080d2985abf71ccdb32aee0cc4c676cb322dc699
Willy <taiwanpride888@gmail.com>
2019-06-20 08:59:37 +0000

    Fix s3 issue

--- Tutorial_Three/V3_training.sh
+++ Tutorial_Three/V3_training.sh
@@ -12,33 +12,42 @@ done
 echo "Start Process"
 echo "Checking for folder"
 
-if [ ! -d "~/.keras/datasets" ]; then
-echo "Creating ~/.keras/datasets"
-mkdir -p ~/.keras/datasets
+if [ ! -d "~/.keras/datasets" ];
+then
+    echo "Creating ~/.keras/datasets"
+    mkdir -p ~/.keras/datasets
+else
+    echo "~/.keras/datasets already exists"
 fi
 
-
-# checking s3 path exist or not
-# if s3 path not exist, try local path
-s3path=/mnt/s3/"$COPYTOFILE"
-ffpath="$COPYTOFILE"
-
-if [ -d "$s3path" ]; then
-  ffpath=$s3path
+if [ -z "$COPYTOFILE" ]
+then
+      echo "s3bucket is not define, result will save in weight folder"
+else
+      echo "result will save in $COPYTOFILE"
+      s3bucket="$COPYTOFILE"
 fi
+
+echo "--------------------"
+echo "----- Download -----"
+echo "--------------------"
+# Install pipenv
+pip install -e git+https://github.com/pypa/pipenv.git@bugfix/resolver-markers#egg=pipenv
+# Clone TWCC-CLI
+git clone https://github.com/TW-NCHC/TWCC-CLI.git
+# cd to TWCC-CLI & Install 
+cd TWCC-CLI && pipenv install
 
 echo "copy data to ~/.keras/datasets/cifar-10-batches-py.tar.gz"
-cp $ffpath/cifar-10-python.tar.gz ~/.keras/datasets/cifar-10-batches-py.tar.gz
-if [ -f ~/.keras/datasets/cifar-10-batches-py.tar.gz ]; then
-  echo "Finished copy data "
-fi
+pipenv run python src/test/s3.py download -s obj0612 -d ~/.keras/datasets/ -k cifar-10-python.tar.gz
+mv ~/.keras/datasets/cifar-10-python.tar.gz ~/.keras/datasets/cifar-10-batches-py.tar.gz
 
 echo "extract cifar-10-batches-py.tar.gz"
 tar -C ~/.keras/datasets/  -xvzf ~/.keras/datasets/cifar-10-batches-py.tar.gz > progress.log
+
 if [ -d ~/.keras/datasets/cifar-10-batches-py ]; then
   echo "Finished extract data "
 fi
-
 
 echo "--------------------"
 echo "----- Training -----"
@@ -47,17 +56,19 @@ echo "--------------------"
 EPOCH=5
 BATCH=45
 
-#if [ ! -d "/mnt/s3/$COPYTOFILE/weights/" ]; then
-#echo "Creating weight folder"
-#mkdir /mnt/s3/"$COPYTOFILE"/weights/
-#fi
+cd ~/AI-Services/Tutorial_Three/inceptionv3/
 
-cd inceptionv3/
+#cd inceptionv3/
 sudo pip install -r requirements.txt >> install.log
 
 cd train/
 
 python V3.py start-training --batch $BATCH --epoch $EPOCH
 
-echo "starting rsync ./weights to $ffpath"
-rsync -av --progress  ./weights $ffpath && echo "Finished Training"
+echo "--------------------"
+echo "------ Upload ------"
+echo "--------------------"
+
+cd ~/AI-Services/Tutorial_Three/TWCC-CLI
+pipenv run python src/test/s3.py upload -s ~/AI-Services/Tutorial_Three/inceptionv3/train/weights/ -d "$COPYTOFILE" -r
+
